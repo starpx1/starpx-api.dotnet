b17f97a8dbcca59dd2bde02a10eb69b3d2d0f61e
qweyrt <chuthoong139@gmail.com>
2024-06-25 22:28:57 +0700

    fixing api responses

--- starpx-mock-api/server.js
+++ starpx-mock-api/server.js
@@ -1,30 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const app = express();
 const PORT = 3000;
-
+var number = 0;
+var generalToken;
 app.use(bodyParser.json());
+let arr = [90, 180];
+let arr2 = [45, 90];
+function random(mn, mx) {
+  return Math.random() * (mx - mn) + mn;
+}
 
 // Mock data
-const mockAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
-const mockPlateSolveResult = {
-  platesolveId: 'platesolve123',
-  solved: true,
-  skyCoordinate: {
-    ra: 180.1,
-    dec: 45.1
-  }
-};
+// const mockAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+// const mockPlateSolveResult = {
+//   platesolveId: 'platesolve' + (int + 1),
+//   solved: true,
+//   skyCoordinate: {
+//     ra: arr[(Math.floor(random(1, 2))) - 1],
+//     dec: arr2[(Math.floor(random(1, 2))) - 1]
+//   }
+// };
+var plateSolveResult;
+function generateAccessToken(payload) {
+  const secretKey = process.env.SECRET_KEY;
 
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+}
 // /authenticate endpoint
 app.post('/authenticate', (req, res) => {
   const apiKey = req.header('apiKey');
   if (apiKey === 'valid-api-key') {
+    const payload = {
+      user_id: 'user123',
+      password: 'Ab@123456',
+      scopes: ['read', 'write']
+    };
+
+    const accessToken = generateAccessToken(payload);
+    generalToken=accessToken;
     res.json({
       apiVersion: '0.1.0',
-      accessToken: mockAccessToken,
-      expiresIn: 57600
+      tokenType: 'Bearer',
+      accessToken: accessToken,
+      expiresIn: 3600
     });
   } else {
     res.status(401).json({
@@ -36,8 +58,21 @@ app.post('/authenticate', (req, res) => {
 // /platesolve endpoint
 app.post('/platesolve', (req, res) => {
   const authHeader = req.header('Authorization');
-  if (authHeader === `Bearer ${mockAccessToken}`) {
-    res.json(mockPlateSolveResult);
+  // const token = authHeader.split(' ')[1];
+  const token = generalToken;
+
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  if (token && decoded) {
+    number=number+1
+    plateSolveResult = {
+      platesolveId: 'platesolve' + number,
+      solved: false,
+      skyCoordinate: {
+        ra: arr[(Math.floor(random(1, 2))) - 1],
+        dec: arr2[(Math.floor(random(1, 2))) - 1]
+      }
+    }
+    res.json(plateSolveResult);
   } else {
     res.status(401).json({
       message: 'Unauthorized - Invalid or missing access token'
@@ -48,9 +83,14 @@ app.post('/platesolve', (req, res) => {
 // /platesolve/:platesolve_id endpoint
 app.get('/platesolve/:platesolve_id', (req, res) => {
   const authHeader = req.header('Authorization');
-  if (authHeader === `Bearer ${mockAccessToken}`) {
-    if (req.params.platesolve_id === 'platesolve123') {
-      res.json(mockPlateSolveResult);
+  // const token = authHeader.split(' ')[1];
+  const token = generalToken;
+
+  const decoded = jwt.verify(token, process.env.SECRET_KEY);
+  if (token && decoded) {
+    if (req.params.platesolve_id === 'platesolve' + number) {
+      plateSolveResult.solved = true
+      res.json(plateSolveResult);
     } else {
       res.status(400).json({
         errorMessage: 'Invalid platesolve ID'
